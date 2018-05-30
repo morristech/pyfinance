6075964226f7fabd70eb5d82fd3a711308c5e356
teddykoker <git@tomkoker.com>
2018-05-29 23:44:12 -0400

    added all sp500 data

--- sp500.py
+++ sp500.py
@@ -1,4 +1,8 @@
 import bs4 as bs
+import datetime as dt
+import os
+import pandas as pd
+import pandas_datareader as web
 import pickle
 import requests
 
@@ -20,5 +24,32 @@ def save_sp500_tickers():
     print(tickers)
     return tickers
 
+# save_sp500_tickers()
 
-save_sp500_tickers()
+
+def get_data_from_yahoo(reload_sp500=False):
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open('sp500.pickle', 'rb') as f:
+            tickers = pickle.load(f)
+
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
+
+    start = dt.datetime(2000, 1, 1)
+    end = dt.datetime(2016, 12, 31)
+
+    for ticker in tickers:
+        print(ticker)
+        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            try:
+                df = web.DataReader(ticker, 'yahoo', start, end)
+                df.to_csv('stock_dfs/{}.csv'.format(ticker))
+            except Exception as e:
+                print(e)
+        else:
+            print('Already have {}'.format(ticker))
+
+
+get_data_from_yahoo()

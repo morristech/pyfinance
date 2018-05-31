6c2a47e5caa206fbc65e0bdb83f0a78c1616b6ce
teddykoker <git@tomkoker.com>
2018-05-30 23:56:38 -0400

    working on getting all data correctly

--- sp500.py
+++ sp500.py
@@ -2,54 +2,83 @@ import bs4 as bs
 import datetime as dt
 import os
 import pandas as pd
+pd.core.common.is_list_like = pd.api.types.is_list_like
 import pandas_datareader as web
 import pickle
 import requests
+import time
 
 
 def save_sp500_tickers():
     resp = requests.get(
-        'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+        "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
 
     soup = bs.BeautifulSoup(resp.text, "lxml")
-    table = soup.find('table', {'class': 'wikitable sortable'})
+    table = soup.find("table", {"class": "wikitable sortable"})
     tickers = []
-    for row in table.findAll('tr')[1:]:  # skip header column
-        ticker = row.findAll('td')[0].text
+    for row in table.findAll("tr")[1:]:  # skip header column
+        ticker = row.findAll("td")[0].text
         tickers.append(ticker)
 
-    with open('sp500.pickle', 'wb') as f:
+    with open("sp500.pickle", "wb") as f:
         pickle.dump(tickers, f)
 
     print(tickers)
     return tickers
-
-# save_sp500_tickers()
 
 
 def get_data_from_yahoo(reload_sp500=False):
     if reload_sp500:
         tickers = save_sp500_tickers()
     else:
-        with open('sp500.pickle', 'rb') as f:
+        with open("sp500.pickle", "rb") as f:
             tickers = pickle.load(f)
 
-    if not os.path.exists('stock_dfs'):
-        os.makedirs('stock_dfs')
+    if not os.path.exists("stock_dfs"):
+        os.makedirs("stock_dfs")
 
     start = dt.datetime(2000, 1, 1)
     end = dt.datetime(2016, 12, 31)
 
     for ticker in tickers:
         print(ticker)
-        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
-            try:
-                df = web.DataReader(ticker, 'yahoo', start, end)
-                df.to_csv('stock_dfs/{}.csv'.format(ticker))
-            except Exception as e:
-                print(e)
+        if not os.path.exists("stock_dfs/{}.csv".format(ticker)):
+            while True:
+                try:
+                    df = web.DataReader(ticker, "google", start, end)
+                except Exception as e:
+                    print(e)
+                    time.sleep(1)
+                else:
+                    df.to_csv("stock_dfs/{}.csv".format(ticker))
+                    break
         else:
-            print('Already have {}'.format(ticker))
+            print("Already have {}".format(ticker))
 
 
-get_data_from_yahoo()
+def compile_data():
+    with open("sp500.pickle", "rb") as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
+    for count, ticker in enumerate(tickers):
+        df = pd.read_csv("stock_dfs/{}.csv".format(ticker))
+        df.set_index("Date", inplace=True)
+        df.rename(columns={"Adj Close": ticker}, inplace=True)
+        df.drop(["Open", "High", "Low", "Close", "Volume"], 1, inplace=True)
+
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how="outer")
+
+        if count % 10 == 0:
+            print(count)
+
+    print(main_df.head())
+    main_df.to_csv("sp500_joined_closes.csv")
+
+
+get_data_from_yahoo(reload_sp500=True)
+
+# compile_data()
